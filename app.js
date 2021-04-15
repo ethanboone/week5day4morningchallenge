52d1588d22700a98b87e0ae308dbd713e4017d6b
ethanboone <eboone2019@gmail.com>
2021-04-15 09:33:51 -0600

    edit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 function triangle(num1, num2, num3) {
-    let triangle = [num1, num2, num3]
-    if (num1 > (num2 + num3) || num2 > (num1 + num3) || num3 > (num1 + num2)) {
+    if (num1 > (num2 + num3) && num2 > (num1 + num3) && num3 > (num1 + num2)) {
         return false
     } else {
         return true
